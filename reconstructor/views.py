1666cd905e20708e73c4b529afa41984afa9cc9f
Adeel Ahmad <adeelahmad14@hotmail.com>
2018-04-09 19:18:33 +0500

    Store image data in database

--- reconstructor/views.py
+++ reconstructor/views.py
@@ -1,12 +1,44 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
+from django.core.files.storage import FileSystemStorage
+from django.utils import timezone
+
+from .models import Image
 
 def index(request):
-    """
-    View function for home page of site.
-    """
-    return render(
-        request,
-        'index.html',
-    )
+	"""
+	View function for home page of site.
+	"""
+	return render(
+		request,
+		'index.html',
+	)
+
+def perform(request):
+	"""
+	Store the images and perform the reconstruction process.
+	"""
+	# Perform validation.
+	if request.method == 'POST':
+		# Get the file.
+		file = request.FILES['files[]']
+
+		# Save the file to a temporary location.
+		fs = FileSystemStorage()
+		filename = fs.save(file.name, file)
+
+		# Create a database entry.
+		image = Image(
+					file_name = filename,
+					file_size = file.size,
+					file_format = file.content_type,
+					session_id = request.session.session_key,
+					date_created = timezone.now
+				)
+		image.save()
+
+		return render(
+			request,
+			'test.html',
+		)
